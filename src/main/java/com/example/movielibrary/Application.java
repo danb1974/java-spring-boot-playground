624e4f4a3978358dc79a8542b1e2c2029323e491
Dan Borlovan <dan.borlovan@datagroup.ro>
2023-03-09 15:54:22 +0200

    Start tcp server for h2 database so we can read schema

--- src/main/java/com/example/movielibrary/Application.java
+++ src/main/java/com/example/movielibrary/Application.java
@@ -1,11 +1,20 @@
 package com.example.movielibrary;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "shutdown")
+    public Server inMemoryH2DatabaseServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
     }
 }
