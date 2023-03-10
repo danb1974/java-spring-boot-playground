8e23271a258fc73788dcf23865959af662e4833b
Dan Borlovan <dan.borlovan@datagroup.ro>
2023-03-10 13:33:15 +0200

    Pass movie id back with modal

--- src/main/java/com/example/movielibrary/controller/FragmentController.java
+++ src/main/java/com/example/movielibrary/controller/FragmentController.java
@@ -3,16 +3,20 @@ package com.example.movielibrary.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 
 @Controller
 public class FragmentController {
-    @GetMapping("/fragment/modal/confirmation")
-    public String confirmationModal(Model model) {
+    @GetMapping("/fragment/modal/confirmation/rental")
+    public String confirmationModal(@RequestParam(name = "movieId") int movieId, Model model) {
         model.addAllAttributes(new HashMap<>() {{
             put("title", "Confirmation");
-            put("content", "Are you sure you want to do something?");
+            put("content", "Are you sure you want to rent this movie?");
+            put("data", new HashMap<String, String>() {{
+                put("movieId", Integer.toString(movieId));
+            }});
         }});
 
         return "modals/confirmation";
