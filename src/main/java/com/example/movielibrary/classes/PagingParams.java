075848959c9da610baa5e57e7415cec3e3e49bad
Dan Borlovan <dan.borlovan@datagroup.ro>
2023-03-13 12:58:07 +0200

    Convert PagingParams to record

--- src/main/java/com/example/movielibrary/classes/PagingParams.java
+++ src/main/java/com/example/movielibrary/classes/PagingParams.java
@@ -4,24 +4,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
-public class PagingParams {
-    private final int page;
-
-    private final int limit;
-
-    public PagingParams(int page, int limit) {
-        this.page = page;
-        this.limit = limit;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public int getLimit() {
-        return limit;
-    }
-
+public record PagingParams(int page, int limit) {
     public Pageable getPageable() {
         Sort sort = Sort.by(Sort.Direction.ASC, "id");
         return getPageable(sort);
